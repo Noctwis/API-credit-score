9bfb2d43cb2086b447b1988ea72e9253d1331450
Ben chelbi Wissem <Ben_chelbiw@yahoo.com>
2023-01-12 10:52:17 +0100

    first

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from io import BytesIO
 from typing import List
 import uvicorn
 from fastapi import FastAPI, File, HTTPException, UploadFile
-from model import load_model, predict, prepare_image
+from model import load_model, predict
 from PIL import Image
 from pydantic import BaseModel
 app = FastAPI()
