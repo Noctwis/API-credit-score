5de1c843b2e77c4b7a9f2584336c349a41ed0a83
Noctwis <60947055+Noctwis@users.noreply.github.com>
2023-03-02 07:38:35 +0100

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -7,12 +7,14 @@ from model import load_model, predict, prepare_cli
 from pydantic import BaseModel
 import json
 import pandas as pd
+from zipfile import ZipFile
 
 app = FastAPI()
 model = load_model()
 # Define the response JSON
 
 class Prediction(BaseModel):
+    #content_type: str
     Pourcentage_de_non_solvabilité: int
     #Pourcentage_de_solvabilité: int
 @app.post("/predict/", response_model=Prediction)
@@ -21,24 +23,27 @@ def prediction(id):
     #if not file.content_type.startswith("image/"):
     #    raise HTTPException(status_code=400, detail="File provided is not an image.")
     #content = await file.read()
-    z = ZipFile("data/X_sample.zip")
-    data = pd.read_csv(z.open('X_sample.csv'), index_col='SK_ID_CURR', encoding ='utf-8')
+    z = ZipFile("X_sample.zip")
+    data = pd.read_csv(z.open('X_sample.csv'), encoding ='utf-8')
     #data = pd.read_csv('X_sample.csv')
-    data2 = data[data['SK_ID_CURR'] == id]
-    js = data2.to_json(orient = 'columns')
-    content = js
+    chk_id = id
+    data2 = data[data['SK_ID_CURR'] == int(chk_id)]
+    #js = data2.to_json()
+    #content = js
     
-    #cli = json.loads(content)
+    #cli = json.loads(js)
         
-    df = pd.read_json(content)
+    #df = pd.read_json(cli, orient ='columns')
     # preprocess the image and prepare it for classification
     #cli = prepare_cli(content)
-    chk_id = df['SK_ID_CURR']
-    sample = df.drop('SK_ID_CURR', axis=1)
+    
+    sample = data2.drop('SK_ID_CURR', axis=1)
     response = predict(sample, chk_id, model)
     #response2 = predict2(sample, chk_id, model)
     # return the response as a JSON
-    return response
+    return { 
+    #"content_type": str(data2), }
+    "Pourcentage_de_non_solvabilité" : response, }
     
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=5000)
